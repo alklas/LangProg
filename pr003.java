f499e0da1bb5485753bdca3f69495122d043fef1
alexander <alklas@mail.ru>
2024-10-28 16:52:14 +0700

    Объекты, вторая версия - с методами внутри класса Cars

--- pr003.java
+++ pr003.java
@@ -2,6 +2,15 @@ class Cars {
 	int pass; //Количество пассажиров
 	int volume; //Ёмкость топливного бака
 	double fuel; //Расход топлива в литрах на 100 км.
+	void range() {
+		System.out.println("расстояние на полном баке: " + (volume/fuel*100));
+	}
+	double range2() {
+		return volume/fuel*100;
+	}
+	double range3(int x) {
+	        return x/fuel*100;
+	}
 }
 class  pr003 {
 	public static void main(String[] args) {
@@ -18,10 +27,14 @@ class  pr003 {
 		System.out.print("Количество пассажиров: " + Honda.pass + " Объём бака: " + Honda.volume);
 		System.out.println(" Расход топлива: " + Honda.fuel);
 
-		double range;
-		range = Honda.volume/Honda.fuel*100;
-		System.out.println("Хонда на полном баке проедет " + range + "км.");
-		range = BMW.volume/BMW.fuel*100;
-		System.out.println("БМВ на полном баке проедет " + range + "км.");
+		//double range;
+		//range = Honda.volume/Honda.fuel*100;
+		System.out.print("Хонда проедет ");
+		Honda.range();
+		System.out.println("Для Хонды: " + Honda.range2());
+		//range = BMW.volume/BMW.fuel*100;
+		System.out.print("БМВ проедет ");
+		BMW.range();
+		System.out.println("Для БМВ c 15 литрами в баке: " +  BMW.range3(15));
 	}
 }
