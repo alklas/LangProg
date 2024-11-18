a21e2e7a308d1c5ca6ed4189151bc8cf930d7268
alexander <alklas@mail.ru>
2024-11-18 17:13:21 +0700

    Добавление модификаторов доступа в класс Queue

--- Queue.java
+++ Queue.java
@@ -1,6 +1,6 @@
 class Queue {
-	char[] q;
-	int putloc, getloc; //указатели на индексы для помещения и извлечения симоволов
+	private char[] q;
+	private int putloc, getloc; //указатели на индексы для помещения и извлечения симоволов
 	Queue(int size) {
 		q = new char[size]; //выделяем память под очередь нужного размера
 		putloc=getloc=0;
@@ -57,5 +57,9 @@ class QDemo {
                                 System.out.print(ch);
                 }
 		System.out.println();
+
+		//Попытка доступа к закрытым переменным класса Queue
+		//bigQ.q[3] = 343;
+		//bigQ.putloc = 3;
 	}
 }
